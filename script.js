7cc495f24f843a379d1bb67825a559fdecd347ae
novecell <novecellofc@gmail.com>
2024-06-17 13:07:38 -0300

    att

---
+++ script.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+  const interval = 5000; // 5 segundos
+  let currentIndex = 0;
+  const items = $('#carouselExampleFade .carousel-item');
+  const itemAmt = items.length;
+
+  function cycleItems() {
+    const item = $('#carouselExampleFade .carousel-item').eq(currentIndex);
+    items.removeClass('active');
+    item.addClass('active');
+  }
+
+  function autoCycle() {
+    currentIndex += 1;
+    if (currentIndex > itemAmt - 1) {
+      currentIndex = 0;
+    }
+    cycleItems();
+  }
+
+  let autoSlide = setInterval(autoCycle, interval);
+
+  $('#carouselExampleFade').on('mouseenter', function() {
+    clearInterval(autoSlide);
+  }).on('mouseleave', function() {
+    autoSlide = setInterval(autoCycle, interval);
+  });
+
+  $('.carousel-control-prev').on('click', function() {
+    currentIndex -= 1;
+    if (currentIndex < 0) {
+      currentIndex = itemAmt - 1;
+    }
+    cycleItems();
+  });
+
+  $('.carousel-control-next').on('click', function() {
+    currentIndex += 1;
+    if (currentIndex > itemAmt - 1) {
+      currentIndex = 0;
+    }
+    cycleItems();
+  });
+
+  cycleItems();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  const ip14Image = document.querySelector('.ip14');
+
+  function handleScroll() {
+    const rect = ip14Image.getBoundingClientRect();
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+    if (rect.top <= windowHeight && rect.bottom >= 0) {
+      ip14Image.classList.add('animate');
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  handleScroll(); // Verifique se a imagem já está visível ao carregar a página
+});
+document.addEventListener('DOMContentLoaded', function() {
+  const ip14Image = document.querySelector('.ip14');
+  const shapeAzulImage = document.querySelector('.shapeazul');
+
+  function handleScroll() {
+    const rect = ip14Image.getBoundingClientRect();
+    const windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+    if (rect.top <= windowHeight && rect.bottom >= 0) {
+      ip14Image.classList.add('animate');
+      shapeAzulImage.classList.add('animate');
+      window.removeEventListener('scroll', handleScroll);
+    }
+  }
+
+  window.addEventListener('scroll', handleScroll);
+  handleScroll(); // Verifique se a imagem já está visível ao carregar a página
+});
